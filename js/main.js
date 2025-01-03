809d22d718dfa46430cee4ab96f50c4eacf7f22b
Amit Sarker <66695572+sarkeramitt@users.noreply.github.com>
2025-01-03 16:51:18 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// @@@@@@@@@@@@ TEXT SLIDER @@@@@@@@@@@@@@
+
+$('.quote-text')
+.not('.slick-intialized')
+.slick({
+    dots : true,
+    prevArrow: false,
+    nextArrow: false,
+    autoplay: true,
+    autoplaySpeed: 2000,
+
+});
